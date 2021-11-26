c4b75f9b2fba7f02973f42bfd7cd5d195aa7a861
Knotoni <65963497+Knotoni@users.noreply.github.com>
2021-11-26 23:25:02 +0300

    Complete redesign

---
+++ js/typedscript.js
@@ -0,0 +1,8 @@
+$(function () {
+    new Typed(".typed", {
+        strings: ["Python developer", "C# developer", "website layout designer", "owl"],
+        typeSpeed: 50,
+        loop: true,
+        cursorChar: "_",
+    });
+});
