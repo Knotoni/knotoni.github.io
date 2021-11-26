264a59d9d2d8bfca2dd6ea59e51ece988e7ddc34
Knotoni <65963497+Knotoni@users.noreply.github.com>
2021-11-26 23:24:33 +0300

    Delete knotoni.github.io directory

--- knotoni.github.io/js/typedscript.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    new Typed(".typed", {
-        strings: ["Python developer", "C# developer", "website layout designer", "owl"],
-        typeSpeed: 50,
-        loop: true,
-        cursorChar: "_",
-    });
-});
