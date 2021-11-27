5aafc0885b20d29301bbb541c33cff1fdf4b24c6
Knotoni <knotonii@gmail.com>
2021-11-28 00:49:21 +0900

    Update typedscript.js

--- js/typedscript.js
+++ js/typedscript.js
@@ -1,6 +1,6 @@
 $(function () {
     new Typed(".typed", {
-        strings: ["a Python developer", "a C# developer", "a website layout designer", "an owl"],
+        strings: ["a Python developer", "a C# developer", "a website layout designer", "an owl", "a Linux server admin (almost)"],
         typeSpeed: 50,
         loop: true,
         cursorChar: "_",
